f9691fdee261b63c2783b2514c7f928bf5edd4a6
alanfg <aherrera@asesoftware.com>
2024-12-20 21:08:28 -0500

    test failx

--- test/test_convertirnumero.py
+++ test/test_convertirnumero.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestConvertirNumero(unittest.TestCase):
     def test_conversion_exitosa(self):
-        self.assertEqual(convertir_numero("10"), 10, "'10' debe convertirse a 10")
+        self.assertEqual(convertir_numero("10"), 13, "'10' debe convertirse a 10")
         self.assertEqual(convertir_numero("-5"), -5, "'-5' debe convertirse a -5")
         self.assertEqual(convertir_numero("0"), 0, "'0' debe convertirse a 0")
 
